be93700b00c3aeb5a1c03643c18c70542ab5dc98
Akbar19 <akbarilxambaev@gmail.com>
2025-06-08 03:52:59 +0500

    Fix duplicate path entry for email token in api_urls.py

--- fin_app_v2/api_urls.py
+++ fin_app_v2/api_urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
      
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),path('api/token/email/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair_email'),
+    path('api/token/email/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair_email'),
     # Job APIs
     path('api/jobs/', api_views.JobListCreateView.as_view(), name='api_job_list'),
     path('api/jobs/<int:pk>/', api_views.JobDetailView.as_view(), name='api_job_detail'),
